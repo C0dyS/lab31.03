7c3a40dc7358245fda700a2fb6f83f951250c6be
C0dyS <sashaculle@gmail.com>
2024-03-21 19:45:35 +0100

    6 task done

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-class Character:
-    def __init__(self, name, health, damage):
-        if not isinstance(name, str):
-            raise TypeError("Name must be a string")
-        if not isinstance(health, int):
-            raise TypeError("Health must be an integer")
-        if not isinstance(damage, int):
-            raise TypeError("Damage must be an integer")
-
+class Student:
+    def __init__(self, name, age, grade, courses):
         self.name = name
-        self.health = health
-        self.damage = damage
+        self.age = age
+        self.grade = grade
+        self.courses = courses
+        if not isinstance(courses, list):
+            raise TypeError("Name must be a list")
 
-    @classmethod
-    def attack(cls,attacker,target):
-        if attacker.damage < target.health:
-            target.health = target.health - attacker.damage
-            print(f'''player {attacker.name} attacks played {target.name}
-he deals {attacker.damage} damage
-player {target.name} is left with {attacker.damage - target.health} health
-                            ''')
-        elif attacker.damage > target.health:
-            target.health = 0
-            print(f'player {attacker.name} kills player {target.name}!')
+    def add_course(self, new_course):
+        self.courses.append(new_course)
 
-a = Character('alex',10,10)
-b = Character('beta',9,10)
-Character.attack(a,b)
-c = Character('gamma',15,15)
-Character.attack(a,c)
+
+a = Student('alex', 19, 4, ['a', 'b', 'c'])
+a.add_course('f')
+print(a.courses)
