e7a45bb5e3de6116852ec53d62a3000f43df7e70
C0dyS <sashaculle@gmail.com>
2024-03-21 19:23:33 +0100

    4 task done

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-class MathClass:
-    @staticmethod
-    def max(*args):
-        if not args:
-            return None
-        largest_number_founder = args[0]
-        for _ in args:
-            if _ > largest_number_founder:
-                largest_number_founder = _
-        return largest_number_founder
-    @staticmethod
-    def min(*args):
-        if not args:
-            return None
-        smallest_number_founder = args[0]
-        for _ in args:
-            if _ < smallest_number_founder:
-                smallest_number_founder = _
-        return smallest_number_founder
-    @staticmethod
-    def factorial(a):
-        result = 1
-        if a < 0:
-            raise ValueError("negative number")
-        elif a == 1:
-            return 1
-        else:
-            for i in range(1,a+1):
-                result *= i
-            return result
-
-    @staticmethod
-    def arithmetic_mean(*args):
-        if not args:
-            return None
-        summ = 0
-        for _ in args:
-            summ += _
-        return summ/len(args)
-
-
-
+class FileUtils:
+    @classmethod
+    def count_lines(cls, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                line_count = sum(1 for line in file)
+            return line_count
+        except FileNotFoundError:
+            print(f"File '{file_path}' not found.")
+            return -1
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return -1
