f707853c18ca17b8b34f3ca29598fb77864ec259
C0dyS <sashaculle@gmail.com>
2024-03-21 19:39:15 +0100

    5task done

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-class FileUtils:
+class Character:
+    def __init__(self, name, health, damage):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if not isinstance(health, int):
+            raise TypeError("Health must be an integer")
+        if not isinstance(damage, int):
+            raise TypeError("Damage must be an integer")
+
+        self.name = name
+        self.health = health
+        self.damage = damage
+
     @classmethod
-    def count_lines(cls, file_path):
-        try:
-            with open(file_path, 'r') as file:
-                line_count = sum(1 for line in file)
-            return line_count
-        except FileNotFoundError:
-            print(f"File '{file_path}' not found.")
-            return -1
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return -1
+    def attack(cls,attacker,target):
+        if attacker.damage < target.health:
+            target.health = target.health - attacker.damage
+            print(f'''player {attacker.name} attacks played {target.name}
+he deals {attacker.damage} damage
+player {target.name} is left with {attacker.damage - target.health} health
+                            ''')
+        elif attacker.damage > target.health:
+            target.health = 0
+            print(f'player {attacker.name} kills player {target.name}!')
+
+a = Character('alex',10,10)
+b = Character('beta',9,10)
+Character.attack(a,b)
+c = Character('gamma',15,15)
+Character.attack(a,c)
