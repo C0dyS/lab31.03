055108e0a8c665544590d872268d491f5e72a98f
C0dyS <sashaculle@gmail.com>
2024-03-21 18:44:24 +0100

    2 task done

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-class Human:
-    _attribute = 0
-
-    def __init__(self):
-        Human._attribute += 1
+class Area_calculator:
+    _amount_of_uses = 0
     @staticmethod
-    def check_amount_of_instances():
-        return Human._attribute
+    def right_angled_triangle_area(b,h):
+        Area_calculator._amount_of_uses += 1
+        return (1/2)*b*h
+    @staticmethod
+    def isosceles_triangle_area(a,b):
+        Area_calculator._amount_of_uses += 1
+        return (1/4)*b*((4*a**2)-b**2)**(1/2)
+    @staticmethod
+    def equilateral_triangle_area(a):
+        Area_calculator._amount_of_uses += 1
+        return (3/(4*a**2))**(1/2)
+    @staticmethod
+    def rectangle_area(a):
+        Area_calculator._amount_of_uses += 1
+        return a**2
+    @staticmethod
+    def romb_area(d_1,d_2):
+        Area_calculator._amount_of_uses +=1
+        return d_1*d_2/2
+    @staticmethod
+    def get_amount_of_uses():
+        return Area_calculator._amount_of_uses
+
+print(Area_calculator.romb_area(1,3))
+print(Area_calculator.get_amount_of_uses())
+print(Area_calculator.romb_area(1,3))
+print(Area_calculator.get_amount_of_uses())
+print(Area_calculator.romb_area(1,3))
+print(Area_calculator.get_amount_of_uses())
 
 
 
-
-
-a = Human()
-b = Human()
-c = Human()
-d = Human()
-print(a.check_amount_of_instances())
-print(b.check_amount_of_instances())
